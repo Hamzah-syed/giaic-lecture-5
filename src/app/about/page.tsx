91cc85f4d2f166e7596b97d9cb5559fb7851461b
hamzah syed <hamzah.syed17@gmail.com>
2024-10-04 20:13:12 +0500

    project init

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home ()  {
+  return (
+    <main className='grid grid-cols-3 bg-slate-400 m-5 '>
+        <div>Hello Grid</div>
+        <div>Hello Grid</div>
+        <div>Hello Grid</div>
+    </main>
+  )
+}
+
